a56d747de092dbf051794cce04feb9ed68974daf
bkapteijns <bram_kapteijns@hotmail.com>
2021-06-26 20:12:52 +0200

    Added nodemailer

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const stripe = require("stripe")(
   "sk_test_51J6XHhGSqSbA8PYSRo15Mh32OmaTfz2bZu7SoQrdZ8sY6pSjbguVXqQLWFeQdwFfMGO7vlYclfNrvKp5bK852lsp00TeJTTvLE"
 );
+const nodemailer = require("nodemailer");
 
 const app = express();
 
@@ -37,6 +38,44 @@ app.post("/create-payment-intent", async (req, res) => {
   res.send({
     clientSecret: paymentIntent.client_secret
   });
+});
+
+app.post("/send-introduction-email", async (req, res) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      type: "login", // other is oauth2
+      user: "trainingplan.fitness@gmail.com",
+      pass: "a1way5 B prot&#ted"
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(
+    {
+      from: '"Training" <trainingplan.fitness@gmail.com>', // sender address
+      to: "bram_kapteijns@hotmail.com", // list of receivers
+      subject: "Testing email", // Subject line
+      text: "This mail has been sent with my app", // plain text body
+      html: "<b>This mail has been sent with my app</b>" // html body
+    },
+    (error, info) => {
+      if (error) return console.error(error);
+      console.log("Message sent: " + JSON.stringify(info));
+    }
+  );
+
+  info && console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  info && console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.send();
 });
 
 app.listen(4000, () => console.log("Node server listening on port 4000!"));
