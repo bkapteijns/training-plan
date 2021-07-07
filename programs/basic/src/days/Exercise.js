3ea5009fa3fb3f6717acff1dc43e307524d3d014
bkapteijns <bram_kapteijns@hotmail.com>
2021-07-07 12:26:45 +0200

    Made program of day 1

--- programs/basic/src/days/Exercise.js
+++ programs/basic/src/days/Exercise.js
@@ -1,22 +1,40 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
+import { Row, Col, ListGroup } from "react-bootstrap";
 // import axios from "axios"; Will be needed when wanting to show a video of the exercise
 
-export default function Exercise({ name, expanded, setExpanded }) {
+export default function Exercise({
+  children,
+  name,
+  amount,
+  expanded,
+  setExpanded
+}) {
   return (
-    <>
-      <ListGroup.Item onClick={setExpanded}>
-        <div>{name}</div>
+    <div style={{ marginBottom: 10 }}>
+      <ListGroup.Item onClick={setExpanded} style={{ cursor: "pointer" }}>
+        <div>
+          {name}, {amount}
+        </div>
       </ListGroup.Item>
       {expanded && (
-        <ListGroup.Item style={{ marginLeft: 10 }}>
-          This is the exercise!
-          <video style={{ height: 360, width: "100%" }} autoPlay loop>
-            <source src="/video.mp4" type="video/mp4" />
+        <ListGroup.Item>
+          <video
+            style={{ maxHeight: 360, width: "100%" }}
+            autoPlay
+            loop
+            onClick={(e) =>
+              e.target.paused ? e.target.play() : e.target.pause()
+            }
+          >
+            <source src={`/videos/${name}.mp4`} type="video/mp4" />
             Sorry, your browser does not accept the video
           </video>
+          <p>Click the video to play/pause</p>
+          <Row style={{ justifyContent: "center" }}>
+            <Col md={10}>{children}</Col>
+          </Row>
         </ListGroup.Item>
       )}
-    </>
+    </div>
   );
 }
